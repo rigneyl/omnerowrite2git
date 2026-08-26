7e2241ab6aeb32cc56a207396ac23d80387497f2
rigneyl <33796668+rigneyl@users.noreply.github.com>
2026-08-26 20:18:10 +1000

    Delete stripe-links.js

--- stripe-links.js
+++
@@ -1,8 +0,0 @@
-/* global window */
-// Stripe-hosted Payment Links. Configure their post-payment redirect to /Support/Thanks/.
-window.OMNERO_STRIPE_LINKS = {
-  aud5: 'https://buy.stripe.com/eVqcN6c6x0EKfxS3Omgfu01',
-  aud10: 'https://buy.stripe.com/5kQcN6daB1IObhC5Wugfu02',
-  aud25: 'https://buy.stripe.com/eVqeVe1rT2MS4TeacKgfu03',
-  custom: 'https://buy.stripe.com/aFa7sMc6xgDI0CYfx4gfu00',
-};
